8787254668757e0e955ae8777ca71b5bc50805c1
p-lacerda <xcloudgameemail@gmail.com>
2022-04-28 15:52:01 -0300

    cria a página de login e o componente de formulário

---
+++ front-end/src/components/Form.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+
+type Props = {};
+
+const Form = (props: Props): JSX.Element => {
+  const router = useRouter();
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const onSubmit = (event: React.FormEvent<EventTarget>) => {
+    event.preventDefault();
+    router.push('/');
+    console.log('yes');
+  }
+
+  const onChange = (event: { target: HTMLInputElement }) => {
+    setData((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value
+    }));
+    
+  }
+
+  const { email, password } = data;
+
+  return (
+    <form onSubmit={ (event) => onSubmit(event)}>
+      <label htmlFor="email">
+        Email
+        <input
+          id="email"
+          name="email"
+          value={email}
+          onChange={(event) => onChange(event)}
+          type="text"
+          placeholder="Seu email"
+        />
+      </label>
+      <label htmlFor="password">
+        Senha
+        <input
+          id="password"
+          name="password"
+          value={password}
+          onChange={(event) => onChange(event)}
+          type="password"
+          placeholder="Sua senha"
+        />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  )
+}
+
+export default Form;
