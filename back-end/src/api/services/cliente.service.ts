a68d128195b187aed125beaf8fdd7246f8cdb2f3
p-lacerda <xcloudgameemail@gmail.com>
2022-04-29 18:07:48 -0300

    finaliza os métodos do service de cliente

--- back-end/src/api/services/cliente.service.ts
+++ back-end/src/api/services/cliente.service.ts
@@ -6,8 +6,25 @@ type bodyCliente = {
 
 export default class ClienteService {
   public async create(name: string) {
+    
+    return Cliente.create(name);
+  }
 
-    const cliente = await Cliente.create(name);
+  public async findAll() {
+    
+    return Cliente.findAll();
+  }
+
+  public async update(id: number, body: bodyCliente) {
+
+    const cliente = await Cliente.findOne({ where: id });
+    
+    return cliente.update(body);
+  }
+
+  public async delete(id: number) {
+
+    const cliente = await Cliente.destroy({ where: id });
     
     return cliente;
   }
