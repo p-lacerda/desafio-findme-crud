c0b1ef40397af96db47480debbd434fed400b48b
p-lacerda <xcloudgameemail@gmail.com>
2022-04-28 22:54:50 -0300

    adiciona o docker e dockerfile para o backend e projeto em geral

--- back-end/src/database/config/database.ts
+++ back-end/src/database/config/database.ts
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   username: process.env.DB_USER || 'admin',
-  password: process.env.DB_PASS || 'admin',
+  password: process.env.DB_PASS || '123456',
   database: process.env.DB_NAME || 'predialx_app',
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || '3002',
