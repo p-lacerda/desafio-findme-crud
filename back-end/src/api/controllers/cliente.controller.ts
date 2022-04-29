4d66caadaa7a91aacc323b69e7982c74ed4a3825
p-lacerda <xcloudgameemail@gmail.com>
2022-04-29 14:10:25 -0300

    adiciona os métodos para a classe ClienteController

---
+++ back-end/src/api/controllers/cliente.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+
+export default class ClienteController {
+  public static async create(req: Request, res: Response) {
+
+  }
+
+  public static async getAll(req: Request, res: Response) {
+
+  }
+
+  public static async edit(req: Request, res: Response) {
+
+  }
+}
