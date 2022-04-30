0c7eed823bf121dfac3d69e3f7cd51ba52d16cfd
p-lacerda <xcloudgameemail@gmail.com>
2022-04-30 17:23:57 -0300

    cria a rota de ordens

--- back-end/src/api/routes/index.ts
+++ back-end/src/api/routes/index.ts
@@ -1,7 +1,9 @@
 import { router as clienteRouter } from './cliente.route';
 import { router as colaboradorRouter } from './colaborador.route';
+import { router as ordensRouter } from './ordens.route';
 
 export {
   clienteRouter,
-  colaboradorRouter
+  colaboradorRouter,
+  ordensRouter
 }
