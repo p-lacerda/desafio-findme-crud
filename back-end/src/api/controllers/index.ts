3e992c5ac9e0cdc077b3e21a682125d3733488bc
p-lacerda <xcloudgameemail@gmail.com>
2022-04-30 13:08:19 -0300

    adiciona o colaborador ao index de controllers

--- back-end/src/api/controllers/index.ts
+++ back-end/src/api/controllers/index.ts
@@ -1,7 +1,10 @@
 import ClienteController from './cliente.controller';
+import ColaboradorController from './coladorador.controller';
 
 const clienteController = new ClienteController();
+const colaboradorController = new ColaboradorController();
 
 export {
-  clienteController
+  clienteController,
+  colaboradorController
 }
