cf819142a96b651ccbd0acb08478547c914bb1dc
p-lacerda <xcloudgameemail@gmail.com>
2022-04-30 17:24:44 -0300

    adiciona ordens no index de services

--- back-end/src/api/services/index.ts
+++ back-end/src/api/services/index.ts
@@ -1,10 +1,13 @@
 import ClienteService from './cliente.service';
 import ColaboradorService from './colaborador.service';
+import OrdensService from './ordens.service';
 
 const clienteService = new ClienteService();
 const colaboradorService = new ColaboradorService();
+const ordensService = new OrdensService();
 
 export {
   clienteService,
-  colaboradorService
+  colaboradorService,
+  ordensService
 }
