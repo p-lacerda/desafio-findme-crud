61ccdf7c239fc040084f2e2da566655ce471aecc
p-lacerda <xcloudgameemail@gmail.com>
2022-05-03 16:04:09 -0300

    configura os arquivos de sequelize e config

--- app/backend/build/database/models/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const databaseConfig = require('../config/database');
-exports.default = new sequelize_1.Sequelize(databaseConfig);
-//# sourceMappingURL=index.js.map
